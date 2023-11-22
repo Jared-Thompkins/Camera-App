2cec046b1e93d7ced0103145b0828290fa1dcd6f
Jared Thompkins <jaredthompkins21@gmail.com>
2023-11-22 14:53:29 -0600

    Initial Commit

---
+++ Camera-App/Camera_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_AppApp.swift
+//  Camera-App
+//
+//  Created by Jared Thompkins on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Camera_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
